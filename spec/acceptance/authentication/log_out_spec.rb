42a7a4b63e144468c9b12f501cfee0bf19745a79
VictoriaMS <vikamag48@gmail.com>
2022-03-28 17:09:20 +0600

    User can to sign out

---
+++ spec/acceptance/authentication/log_out_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'user log out', %q{
+  In order ensure the security of personal data
+  As a authenticated user
+  I want to able to log out
+} do 
+  given(:user) { create(:user) }
+  scenario 'authenticated user log out' do 
+    log_in(user)
+    click_on 'Sign out'
+
+    expect(page).to have_content 'Signed out successfully'
+  end
+end
