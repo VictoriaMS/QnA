32b123e3bceadf767a9fafbe18e7fc53103136ea
VictoriaMS <vikamag48@gmail.com>
2022-04-18 16:47:54 +0600

    Author of the answer cannot vote for own answer

---
+++ spec/acceptance/answers/vote_for_answer_spec.rb
@@ -0,0 +1,59 @@
+require 'acceptance/acceptance_helper'
+
+feature 'vote for answer', %q{
+  In order to highlight a useful or not useful answer
+  As a authenticated user
+  I want to be able to vote for answer
+} do
+
+  let(:user)      { create(:user) }
+  let(:author)    { create(:user) }
+  let!(:question) { create(:question) }
+  let!(:answer)   { create(:answer, question: question, user: author) } 
+
+  describe 'Authenticated user'  do 
+    before do
+      log_in(user)
+      visit question_path(question)
+    end
+
+    scenario 'votes up for answer', js: true do
+      within '.answers' do
+        click_on 'Vote up'
+  
+        within '.raiting' do
+          expect(page).to have_content '1'
+        end     
+      end
+    end
+  
+    scenario 'votes down for answer', js: true do
+      within '.answers' do
+        click_on 'Vote down'
+  
+        within '.raiting' do 
+          expect(page).to have_content '-1'
+        end
+      end
+    end
+  end
+
+  describe 'Author of the answer', js: true do
+    before do 
+      log_in(author)
+      visit question_path(question)
+    end
+
+    scenario 'votes up for own answer' do
+      within '.answers' do
+        expect(page).to_not have_link 'Vote up'
+      end
+    end
+
+    scenario 'votes down for own answer' do
+      within '.answers' do
+        expect(page).to_not have_link 'Vote down'
+      end
+    end
+  end 
+end
