eddbfd759dbbaf93e7f33bf082b0e568f6886ac5
VictoriaMS <vikamag48@gmail.com>
2022-05-03 22:01:34 +0600

    AttachmentsCOntroller with responders

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,7 +1,15 @@
 class AttachmentsController < ApplicationController
+  before_action :set_attachment, only: :destroy
+
+  respond_to :js, only: :destroy
+
   def destroy 
+    respond_with(@attachment.destroy)
+  end
+
+  private 
+
+  def set_attachment 
     @attachment = Attachment.find(params[:id])
-    @attachable = @attachment.attachable
-    @attachment.destroy
   end
 end
