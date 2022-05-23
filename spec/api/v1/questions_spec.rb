71590d696be29ff26b6a8004b3dc2b914ce7fc21
VictoriaMS <vikamag48@gmail.com>
2022-05-23 21:42:20 +0600

    Implemented api for a separate question, which contains comments and attachments in the form of urls

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -36,4 +36,67 @@ describe 'Questions API' do
       end 
     end
   end
+
+  describe 'GET /show' do 
+    context 'unauthorized' do 
+      let(:question) { create(:question) }
+
+      it 'returns 401 status if there is no acess_token' do 
+        get "/api/v1/questions/#{ question.id }", params: { format: :json }
+        expect(response.status).to eq 401 
+      end
+
+      it 'returns 401 status if access_token is invalid' do 
+        get "/api/v1/questions/#{ question.id }", params: { format: :json, access_token: '1234'}
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do 
+      let(:question)     { create(:question) }
+      let(:access_token) { create(:access_token) }
+      let!(:comments)    { create_list(:comment, 3, commentable: question) }
+      let!(:attachments) { create_list(:attachment, 2, attachable: question) }
+
+      before { get "/api/v1/questions/#{question.id}", params: { format: :json, access_token: access_token.token } } 
+
+      it 'returns 200 status' do 
+        expect(response).to be_success
+      end
+
+      %w(id title body created_at updated_at).each do |attr|
+        it "question object contains #{ attr }" do 
+          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("question/#{attr}")
+        end
+      end 
+
+      context 'comments' do 
+        it 'included in question object' do 
+          expect(response.body).to have_json_size(comments.size).at_path("question/comments")
+        end 
+        
+        %w(id body created_at updated_at commentable_id commentable_type user_id).each do |attr|
+          it "comment object contains #{ attr }" do 
+            expect(response.body).to be_json_eql(comments.first.send(attr.to_sym).to_json).at_path("question/comments/0/#{ attr }")
+          end
+        end
+      end
+
+      context 'attachments' do 
+        it 'included in question object' do 
+          expect(response.body).to have_json_size(attachments.size).at_path("question/attachments")
+        end
+
+        it 'attachment object contains url' do
+          expect(response.body).to be_json_eql(attachments.first.file.url.to_json).at_path("question/attachments/0/url")
+        end
+
+        %w(id file created_at updated_at attachment_id attachment_type).each do |attr|
+          it "attachment object does not contains #{ attr }" do 
+           expect(response.body).to_not have_json_path("question/attachments/0/#{ attr }")
+        end
+        end
+      end
+    end
+  end
 end
