f70fd3fb5eb09686aa7d822c4d19ead2a3be307a
VictoriaMS <vikamag48@gmail.com>
2022-05-26 17:24:20 +0600

    Refactoring

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,24 +1,25 @@
 class Api::V1::AnswersController < Api::V1::BaseController
+  before_action :set_question, only: [:index, :show]
   authorize_resource
   
   def index 
-    question = Question.find(params[:question_id])
-    @answers = question.answers
-    respond_with @answers, each_serializer: AnswersSerializer
+    respond_with @question.answers, each_serializer: AnswersSerializer
   end
 
   def show
-    @answer = Answer.find(params[:id])
-    respond_with @answer, serializer: AnswerSerializer
+    respond_with Answer.find(params[:id]), serializer: AnswerSerializer
   end
 
   def create
-    question = Question.find(params[:question_id])
-    @answer = question.answers.create(answer_params)
-    respond_with @answer
+    @answer = @question.answers.create(answer_params)
+    respond_with @answer, serializer: AnswerSerializer
   end
 
   private
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
 
   def answer_params
     params.require(:answer).permit(:body, :user_id)
--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -2,13 +2,11 @@ class Api::V1::QuestionsController < Api::V1::BaseController
   authorize_resource
   
   def index 
-    @questions = Question.all
-    respond_with @questions, each_serializer: QuestionsSerializer
+    respond_with Question.all, each_serializer: QuestionsSerializer
   end
 
   def show 
-    @question = Question.find(params[:id])
-    respond_with @question, serializer: QuestionSerializer
+    respond_with Question.find(params[:id]), serializer: QuestionSerializer
   end
 
   def create
