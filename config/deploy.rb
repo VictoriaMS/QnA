135457c91e82796cb6155c9e430fb8ae1b84034b
VictoriaMS <vikamag48@gmail.com>
2022-06-13 20:54:52 +0600

    Change master on main

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock "~> 3.17.0"
 
 set :application, "qna"
 set :repo_url, "git@github.com:VictoriaMS/QnA.git"
+set :branch, 'main'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
